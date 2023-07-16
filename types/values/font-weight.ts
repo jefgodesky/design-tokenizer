699a9720711a3eea98c96643608ab3aa65791fee
Jason Godesky <jefgodesky@gmail.com>
2023-07-16 09:46:27 -0400

    Add new type: FontWeight

---
+++ types/values/font-weight.ts
@@ -0,0 +1,3 @@
+type FontWeight = number | 'thin' | 'hairline' | 'extra-light' | 'ultra-light' | 'light' | 'normal' | 'regular' | 'book' | 'medium' | 'semi-bold' | 'demi-bold' | 'bold' | 'extra-bold' | 'ultra-bold' | 'black' | 'heavy' | 'extra-black' | 'ultra-black'
+
+export default FontWeight

c321f4f6e846f03ed3764d307d3dbeafccdefe4e
Jason Godesky <jefgodesky@gmail.com>
2023-07-16 13:12:19 -0400

    Add new testing function: isColorHex

--- types/basic/color-hex.spec.ts
+++ types/basic/color-hex.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { isHexit } from './color-hex.js'
+import { isHexit, isColorHex } from './color-hex.js'
 
 describe('ColorHex', () => {
   describe('isHexit', () => {
@@ -53,6 +53,72 @@ describe('ColorHex', () => {
 
     it('returns false for an object with a valid property', () => {
       expect(isHexit({ hex: '0' })).to.equal(false)
+    })
+  })
+
+  describe('isColorHex', () => {
+    it('returns false for undefined', () => {
+      expect(isColorHex(undefined)).to.equal(false)
+    })
+
+    it('returns false for null', () => {
+      expect(isColorHex(null)).to.equal(false)
+    })
+
+    it('returns false for functions', () => {
+      expect(isColorHex(() => {})).to.equal(false)
+    })
+
+    it('returns false for true', () => {
+      expect(isColorHex(true)).to.equal(false)
+    })
+
+    it('returns false for false', () => {
+      expect(isColorHex(false)).to.equal(false)
+    })
+
+    it('returns false for numbers', () => {
+      expect(isColorHex(1)).to.equal(false)
+    })
+
+    it('returns true for a 24 bit hexadecimal RGB value', () => {
+      expect(isColorHex('#ff0000')).to.equal(true)
+    })
+
+    it('returns true for a 24+8 bit hexadecimal RGB value', () => {
+      expect(isColorHex('#ff0000ff')).to.equal(true)
+    })
+
+    it('returns false for a 12 bit hexadecimal RGB value', () => {
+      expect(isColorHex('#f00')).to.equal(false)
+    })
+
+    it('returns false for a string that doesn\'t begin with an octothorpe', () => {
+      expect(isColorHex('ff0000')).to.equal(false)
+    })
+
+    it('returns false for a string that isn\'t a hexadecimal value', () => {
+      expect(isColorHex('#ffgghh')).to.equal(false)
+    })
+
+    it('returns false for any other string', () => {
+      expect(isColorHex('red')).to.equal(false)
+    })
+
+    it('returns false for an empty array', () => {
+      expect(isColorHex([])).to.equal(false)
+    })
+
+    it('returns false for an array of otherwise valid items', () => {
+      expect(isColorHex(['#ff0000'])).to.equal(false)
+    })
+
+    it('returns false for an empty object', () => {
+      expect(isColorHex({})).to.equal(false)
+    })
+
+    it('returns false for an object with a valid property', () => {
+      expect(isColorHex({ color: '#ff0000' })).to.equal(false)
     })
   })
 })
--- types/basic/color-hex.ts
+++ types/basic/color-hex.ts
@@ -6,5 +6,14 @@ const isHexit = (obj: any): boolean => {
   return valid.includes(obj)
 }
 
+const isColorHex = (obj: any): boolean => {
+  if (typeof obj !== 'string') return false
+  const validLengths = [7, 9]
+  const chars = obj.split('')
+  if (!validLengths.includes(chars.length)) return false
+  if (chars[0] !== '#') return false
+  return chars.slice(1).reduce((acc, curr) => acc && isHexit(curr), true)
+}
+
 export default ColorHex
-export { isHexit }
+export { isHexit, isColorHex }

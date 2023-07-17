2a0610d900ac8c8724bab5b57e20313034c33217
Jason Godesky <jefgodesky@gmail.com>
2023-07-17 10:56:11 -0400

    DictionaryValue: Expand to handle defined token types
    We disallow reserved property names (that start with "$"), but then we specifically allow those that are used in defined token types when they're used properly.

--- types/dictionary-value.spec.ts
+++ types/dictionary-value.spec.ts
@@ -73,4 +73,116 @@ describe('isDictionaryValue', () => {
   it('returns false if a dictionary includes reserved keys', () => {
     expect(isDictionaryValue({ $a: 1 })).to.equal(false)
   })
+
+  it('returns true for a nested color token', () => {
+    expect(isDictionaryValue({ color: { green: { $type: 'color', $value: '#008800' } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed color token', () => {
+    expect(isDictionaryValue({ color: { green: { $type: 'color', $value: '#080' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested dimension token', () => {
+    expect(isDictionaryValue({ spacing: { vertical: { $type: 'dimension', $value: '1rem' } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed dimension token', () => {
+    expect(isDictionaryValue({ spacing: { vertical: { $type: 'dimension', $value: '1em' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested font family token', () => {
+    expect(isDictionaryValue({ font: { basic: { $type: 'fontFamily', $value: ['Helvetica', 'sans-serif'] } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed font family token', () => {
+    expect(isDictionaryValue({ font: { basic: { $type: 'font-family', $value: ['Helvetica', 'sans-serif'] } } })).to.equal(false)
+  })
+
+  it('returns true for a nested font weight token', () => {
+    expect(isDictionaryValue({ weight: { normal: { $type: 'fontWeight', $value: 'normal' } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed font weight token', () => {
+    expect(isDictionaryValue({ weight: { normal: { $type: 'font-weight', $value: 'normal' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested duration token', () => {
+    expect(isDictionaryValue({ duration: { mid: { $type: 'duration', $value: '1000ms' } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed duration token', () => {
+    expect(isDictionaryValue({ duration: { mid: { $type: 'duration', $value: '1s' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested cubic bézier token', () => {
+    expect(isDictionaryValue({ curves: { flat: { $type: 'cubicBezier', $value: [0, 0, 0, 0] } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed cubic bézier token', () => {
+    expect(isDictionaryValue({ curves: { flat: { $type: 'cubic-bezier', $value: [0, 0, 0, 0] } } })).to.equal(false)
+  })
+
+  it('returns true for a nested number token', () => {
+    expect(isDictionaryValue({ numbers: { loneliest: { $type: 'number', $value: 1 } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed number token', () => {
+    expect(isDictionaryValue({ numbers: { loneliest: { $type: 'number', $value: 'one' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested stroke style string token', () => {
+    expect(isDictionaryValue({ stroke: { basic: { $type: 'strokeStyle', $value: 'dashed' } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed stroke style string token', () => {
+    expect(isDictionaryValue({ stroke: { loneliest: { $type: 'strokeStyle', $value: 'dashing' } } })).to.equal(false)
+  })
+
+  it('returns true for a nested stroke style object token', () => {
+    expect(isDictionaryValue({ stroke: { basic: { $type: 'strokeStyle', $value: { dashArray: ['0.5rem', '0.25rem'], lineCap: 'round' } } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed stroke style object token', () => {
+    expect(isDictionaryValue({ stroke: { basic: { $type: 'strokeStyle', $value: { dashArray: ['0.5rem', '0.25rem'], lineCap: 'pop' } } } })).to.equal(false)
+  })
+
+  it('returns true for a nested border token', () => {
+    expect(isDictionaryValue({ stroke: { basic: { $type: 'border', $value: { color: '#008800', width: '1px', style: 'solid' } } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed border token', () => {
+    expect(isDictionaryValue({ stroke: { basic: { $type: 'border', $value: { color: '#008800', width: '1px', style: 'jazzy' } } } })).to.equal(false)
+  })
+
+  it('returns true for a nested transition token', () => {
+    expect(isDictionaryValue({ transition: { mid: { $type: 'transition', $value: { duration: '1000ms', delay: '0ms', timingFunction: [0, 0, 0, 0] } } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed transition token', () => {
+    expect(isDictionaryValue({ transition: { mid: { $type: 'transition', $value: { duration: '1000ms', delay: '0s', timingFunction: [0, 0, 0, 0] } } } })).to.equal(false)
+  })
+
+  it('returns true for a nested shadow token', () => {
+    expect(isDictionaryValue({ shadow: { basic: { $type: 'shadow', $value: { color: '#00000080', offsetX: '0.5rem', offsetY: '0.5rem', blur: '1.5rem', spread: '0rem' } } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed shadow token', () => {
+    expect(isDictionaryValue({ shadow: { basic: { $type: 'shadow', $value: { color: '#00000080', offsetX: '0.5rem', offsetY: '0.5rem', blur: '1.5rem', spread: 0 } } } })).to.equal(false)
+  })
+
+  it('returns true for a nested gradient token', () => {
+    expect(isDictionaryValue({ gradient: { monochrome: { $type: 'gradient', $value: [{ color: '#000000', position: 0 }, { color: '#ffffff', position: 1 }] } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed gradient token', () => {
+    expect(isDictionaryValue({ gradient: { monochrome: { $type: 'gradient', $value: [{ color: '#000', position: 0 }, { color: '#ffffff', position: 1 }] } } })).to.equal(false)
+  })
+
+  it('returns true for a nested typography token', () => {
+    expect(isDictionaryValue({ gradient: { monochrome: { $type: 'typography', $value: { fontFamily: ['Helvetica', 'sans-serif'], fontSize: '1rem', fontWeight: 'normal', letterSpacing: '0rem', lineHeight: 1.2 } } } })).to.equal(true)
+  })
+
+  it('returns false for a malformed gradient token', () => {
+    expect(isDictionaryValue({ gradient: { monochrome: { $type: 'typography', $value: { fontFamily: ['Helvetica', 'sans-serif'], fontSize: '1rem', fontWeight: 'normal', letterSpacing: 0, lineHeight: 1.2 } } } })).to.equal(false)
+  })
 })
--- types/dictionary-value.ts
+++ types/dictionary-value.ts
@@ -1,11 +1,36 @@
+import BorderToken, { isBorderToken } from './tokens/border.js'
+import ColorToken, { isColorToken } from './tokens/color.js'
+import CubicBezierToken, { isCubicBezierToken } from './tokens/cubic-bezier.js'
 import Dictionary, { isDictionary } from './dictionary.js'
+import DimensionToken, { isDimensionToken } from './tokens/dimension.js'
+import DurationToken, { isDurationToken } from './tokens/duration.js'
+import FontFamilyToken, { isFontFamilyToken } from './tokens/font-family.js'
+import FontWeightToken, { isFontWeightToken } from './tokens/font-weight.js'
+import GradientToken, { isGradientToken } from './tokens/gradient.js'
+import NumberToken, { isNumberToken } from './tokens/number.js'
+import ShadowToken, { isShadowToken } from './tokens/shadow.js'
+import StrokeStyleToken, { isStrokeStyleToken } from './tokens/stroke-style.js'
+import TransitionToken, { isTransitionToken } from './tokens/transition.js'
+import TypographyToken, { isTypographyToken } from './tokens/typography.js'
 
-type DictionaryValue = Dictionary | string | number | boolean | DictionaryValue[]
+type DictionaryValuePrimitives = Dictionary | string | number | boolean
+type FontTokenTypes = FontFamilyToken | FontWeightToken
+type BasicTokenTypes = ColorToken | DimensionToken | FontTokenTypes | DurationToken | CubicBezierToken | NumberToken
+type CompositeTokenTypes = StrokeStyleToken | BorderToken | TransitionToken | ShadowToken | GradientToken | TypographyToken
+type DictionaryValue = DictionaryValuePrimitives | BasicTokenTypes | CompositeTokenTypes | DictionaryValue[]
 
 const isDictionaryValue = (obj: any): boolean => {
   const types = ['boolean', 'number', 'string']
   if (types.includes(typeof obj)) return true
-  if (isDictionary(obj)) return true
+
+  const tests = [isColorToken, isDimensionToken, isFontFamilyToken,
+    isFontWeightToken, isDurationToken, isCubicBezierToken, isNumberToken,
+    isStrokeStyleToken, isBorderToken, isTransitionToken, isShadowToken,
+    isGradientToken, isTypographyToken, isDictionary]
+  for (const test of tests) {
+    if (test(obj)) return true
+  }
+
   return Array.isArray(obj) && obj.reduce((acc: boolean, curr: any) => acc && isDictionaryValue(curr), true)
 }
 

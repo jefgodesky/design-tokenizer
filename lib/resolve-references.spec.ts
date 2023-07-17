6c4ace555fa2e40fad0b5f454e2ab697e290bab6
Jason Godesky <jefgodesky@gmail.com>
2023-07-17 11:43:29 -0400

    Add new method: resolveReferences

---
+++ lib/resolve-references.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai'
+import Dictionary from './types/dictionary.js'
+import resolveReferences from './resolve-references.js'
+
+describe('resolveReferences', () => {
+  it('resolves references in a flat value dictionary', () => {
+    const values: Dictionary = {
+      'color.green': '#008800',
+      'font.family.basic': ['Helvetica', 'sans-serif'],
+      'color.accent': '{color.green}',
+      'font.heading': '{font.family.basic}'
+    }
+    const actual = resolveReferences(values)
+    expect(actual['color.accent']).to.equal(values['color.green'])
+    expect(actual['font.heading']).to.eql(values['font.family.basic'])
+  })
+})
---
+++ lib/resolve-references.ts
@@ -0,0 +1,17 @@
+import rfdc from 'rfdc'
+import Dictionary from './types/dictionary.js'
+
+const clone = rfdc()
+
+const resolveReferences = (values: Dictionary): Dictionary => {
+  const cpy = clone(values)
+  const regex = /{(.*?)}/
+  for (const key in cpy) {
+    const match = typeof cpy[key] === 'string' ? (cpy[key] as string).match(regex) : null
+    if (match === null || match.length < 2) continue
+    cpy[key] = cpy[match[1]]
+  }
+  return cpy
+}
+
+export default resolveReferences

5c9560f3b5915e899f5199784521e8277c0dba26
Jason Godesky <jefgodesky@gmail.com>
2023-07-18 16:25:17 -0400

    Add new type: DerefToken
    Alias for any of our dereferenced token types.

---
+++ lib/types/deref.ts
@@ -0,0 +1,17 @@
+import DerefBorderToken from './tokens/dereferenced/border.js'
+import DerefColorToken from './tokens/dereferenced/color.js'
+import DerefCubicBezierToken from './tokens/dereferenced/cubic-bezier.js'
+import DerefDimensionToken from './tokens/dereferenced/dimension.js'
+import DerefDurationToken from './tokens/dereferenced/duration.js'
+import DerefFontFamilyToken from './tokens/dereferenced/font-family.js'
+import DerefFontWeightToken from './tokens/dereferenced/font-weight.js'
+import DerefGradientToken from './tokens/dereferenced/gradient.js'
+import DerefNumberToken from './tokens/dereferenced/number.js'
+import DerefShadowToken from './tokens/dereferenced/shadow.js'
+import DerefStrokeStyleToken from './tokens/dereferenced/stroke-style.js'
+import DerefTransitionToken from './tokens/dereferenced/transition.js'
+import DerefTypographyToken from './tokens/dereferenced/typography.js'
+
+type DerefToken = DerefBorderToken | DerefColorToken | DerefCubicBezierToken | DerefDimensionToken | DerefDurationToken | DerefFontFamilyToken | DerefFontWeightToken | DerefGradientToken | DerefNumberToken | DerefShadowToken | DerefStrokeStyleToken | DerefTransitionToken | DerefTypographyToken
+
+export default DerefToken

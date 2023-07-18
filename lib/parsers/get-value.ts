4e77c8ac7cbac5bb024a4a767367dde4b6da14ea
Jason Godesky <jefgodesky@gmail.com>
2023-07-18 16:18:47 -0400

    Remove Dictionary types
    And our parser functions with them.

--- lib/parsers/get-value.ts
+++
@@ -1,3 +0,0 @@
-const getValue = (dict: { [key: string]: any }, key: string): any => dict[key]?.$value
-
-export default getValue
--- lib/types/dictionary-key.ts
+++
@@ -1,8 +0,0 @@
-type DictionaryKey = Exclude<string, `$${string}`>
-
-const isDictionaryKey = (obj: any): boolean => {
-  return typeof obj === 'string' && !obj.startsWith('$')
-}
-
-export default DictionaryKey
-export { isDictionaryKey }

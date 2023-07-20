8021c568c28c5be2f99af896fc7ea9fa356864ad
Jason Godesky <jefgodesky@gmail.com>
2023-07-20 15:45:25 -0400

    renderTypographyToken: Render tokens that include fontStyle renderTypographyToken: Refactoring

--- lib/renderers/scss/tokens/typography.ts
+++ lib/renderers/scss/tokens/typography.ts
@@ -2,12 +2,17 @@ import DerefTypographyToken from '../../../types/tokens/dereferenced/typography.
 import transformFontFamily from '../transformers/font-family.js'
 
 const renderTypographyToken = (name: string, token: DerefTypographyToken): string => {
-  const { fontFamily, fontSize, fontWeight, letterSpacing, lineHeight } = token.$value
+  const { fontFamily, fontSize, fontStyle, fontWeight, letterSpacing, lineHeight } = token.$value
   const units = fontSize.endsWith('rem') ? 'rem' : 'px'
   const size = parseInt(fontSize.substring(0, fontSize.indexOf(units)))
-  const fontSizeDeclaration = isNaN(size) ? fontSize : `${fontSize}/${size * lineHeight}${units}`
+  const elems = [
+    fontStyle,
+    fontWeight,
+    isNaN(size) ? fontSize : `${fontSize}/${size * lineHeight}${units}`,
+    transformFontFamily(fontFamily)
+  ].filter(elem => elem !== undefined)
   return [
-    `$${name}: ${fontWeight} ${fontSizeDeclaration} ${transformFontFamily(fontFamily)};`,
+    `$${name}: ${elems.join(' ')};`,
     `$${name}-letter-spacing: ${letterSpacing};`
   ].join('\n')
 }

4e22a412fb9bdf165e3cbd09bc24b65597826c3a
Jason Godesky <jefgodesky@gmail.com>
2023-07-20 13:20:21 -0400

    DerefBorder needs to use DerefStrokeStyleObject, not StrokeStyleObject

--- lib/types/composite/dereferenced/border.ts
+++ lib/types/composite/dereferenced/border.ts
@@ -2,18 +2,18 @@ import Border, { isBorder } from '../border.js'
 import ColorHex, { isColorHex } from '../../basic/color-hex.js'
 import Dimension, { isDimension } from '../../basic/dimension.js'
 import StrokeStyleString, { isStrokeStyleString } from '../../basic/stroke-style.js'
-import StrokeStyleObject, { isStrokeStyleObject } from '../stroke-style.js'
+import DerefStrokeStyleObject, { isDerefStrokeStyleObject } from './stroke-style.js'
 
 interface DerefBorder extends Omit<Border, 'color' | 'width' | 'style'> {
   color: ColorHex
   width: Dimension
-  style: StrokeStyleString | StrokeStyleObject
+  style: StrokeStyleString | DerefStrokeStyleObject
 }
 
 const isDerefBorder = (obj: any): obj is DerefBorder => {
   if (!isBorder(obj)) return false
   const { color, width, style } = obj
-  return isColorHex(color) && isDimension(width) && (isStrokeStyleString(style) || isStrokeStyleObject(style))
+  return isColorHex(color) && isDimension(width) && (isStrokeStyleString(style) || isDerefStrokeStyleObject(style))
 }
 
 export default DerefBorder

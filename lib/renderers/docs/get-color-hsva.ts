a2d0b00c7476b1037f4fd17008d6032d2c862b06
Jason Godesky <jefgodesky@gmail.com>
2023-07-21 18:03:38 -0400

    Add new method: getColorHSVA

---
+++ lib/renderers/docs/get-color-hsva.ts
@@ -0,0 +1,9 @@
+import { colord } from 'colord'
+import DerefColorToken from '../../types/tokens/dereferenced/color.js'
+
+const getColorHSVA = (token: DerefColorToken): string => {
+  const hsva = colord(token.$value).toHsv()
+  return `${hsva.h}º, ${hsva.s}, ${hsva.v}, ${hsva.a}`
+}
+
+export default getColorHSVA

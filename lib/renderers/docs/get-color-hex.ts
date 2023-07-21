bcb4e563481f1060b850629fc69bcdedf279e032
Jason Godesky <jefgodesky@gmail.com>
2023-07-21 17:27:33 -0400

    Add new method: getColorHex

---
+++ lib/renderers/docs/get-color-hex.ts
@@ -0,0 +1,6 @@
+import ColorHex from '../../types/basic/color-hex.js'
+import DerefColorToken from '../../types/tokens/dereferenced/color.js'
+
+const getColorHex = (token: DerefColorToken): ColorHex => token.$value
+
+export default getColorHex

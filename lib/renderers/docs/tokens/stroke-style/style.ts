021f0ef50542de9205f4c650dcdff6406e7f2551
Jason Godesky <jefgodesky@gmail.com>
2023-07-22 14:44:40 -0400

    Refactoring getStrokeStyle

--- lib/renderers/docs/tokens/stroke-style/style.ts
+++ lib/renderers/docs/tokens/stroke-style/style.ts
@@ -1,9 +1,6 @@
 import DerefStrokeStyleToken from '../../../../types/tokens/dereferenced/stroke-style.js'
 import getStyle from '../../types/stroke-style/style.js'
 
-const getStrokeStyle = (token: DerefStrokeStyleToken): string => {
-  const { $value } = token
-  return getStyle($value)
-}
+const getStrokeStyle = (token: DerefStrokeStyleToken): string => getStyle(token.$value)
 
 export default getStrokeStyle

370bec10f6a7ba160cedafdba43d22ef70c4467f
Jason Godesky <jefgodesky@gmail.com>
2023-07-22 11:17:50 -0400

    Add new method: getFontWeight

---
+++ lib/renderers/docs/get-font-weight.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import DerefFontWeightToken from '../../types/tokens/dereferenced/font-weight.js'
+import getFontWeight from './get-font-weight.js'
+
+describe('getFontWeight', () => {
+  it('returns the font weight string', () => {
+    const token: DerefFontWeightToken = { $type: 'fontWeight', $value: 'solid' }
+    expect(getFontWeight(token)).to.equal('solid')
+  })
+
+  it('returns the font weight number as a string', () => {
+    const token: DerefFontWeightToken = { $type: 'fontWeight', $value: 400 }
+    expect(getFontWeight(token)).to.equal('400')
+  })
+})

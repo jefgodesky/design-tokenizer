5ba7a27d7ee1ec84f848e442a9bfd034446da917
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 14:15:39 -0400

    Colophon: Refactor to use ColophonEntry

--- lib/types/colophon.ts
+++ lib/types/colophon.ts
@@ -1,22 +1,13 @@
+import ColophonEntry, { isColophonEntry } from './colophon-entry.js'
 import isObject from './guards/object.js'
 
 interface Colophon {
-  [key: string]: {
-    name: string
-    designer: string
-    url: string
-  }
+  [key: string]: ColophonEntry
 }
 
 const isColophon = (obj: any): obj is Colophon => {
   if (!isObject(obj)) return false
-  for (const key in obj) {
-    if (!isObject(obj[key])) return false
-    const { name, designer, url } = obj[key]
-    const check = [name, designer, url].reduce((acc: boolean, curr: any) => acc && typeof curr === 'string', true)
-    if (!check) return false
-  }
-  return true
+  return Object.keys(obj).reduce((acc: boolean, key: string) => acc && isColophonEntry(obj[key]), true)
 }
 
 export default Colophon

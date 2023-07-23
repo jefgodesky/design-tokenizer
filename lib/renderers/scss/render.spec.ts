bc881f974bf2e3dde7ad7172a07e5cd3ef90e5fb
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 13:31:41 -0400

    renderSCSS: Use $extensions instead of YAML

--- lib/renderers/scss/render.spec.ts
+++ lib/renderers/scss/render.spec.ts
@@ -14,24 +14,21 @@ describe('renderSCSS', () => {
   after(() => { mock.restore() })
 
   it('renders the tokens to an SCSS file', () => {
-    const list: DerefTokenList = { 'color.green': { $type: 'color', $value: '#008800' } }
-    renderSCSS(list, { 'color.green': { file: 'scss/modules/_colors.scss' } }, false)
+    const list: DerefTokenList = { 'color.green': { $type: 'color', $value: '#008800', $extensions: { 'com.npmjs.package.design-tokenizer': { scss: { file: 'scss/modules/_colors.scss', variable: 'green' } } } } }
+    renderSCSS(list, false)
     const actual = readFileSync('scss/modules/_colors.scss', { encoding: 'utf8' })
-    expect(actual).to.equal('$color-green: #008800;')
+    expect(actual).to.equal('$green: #008800; // color.green')
   })
 
   it('renders differently prefixed tokens to different files', () => {
     const list: DerefTokenList = {
-      'color.green': { $type: 'color', $value: '#008800' },
-      'breakpoint.primary': { $type: 'dimension', $value: '40rem' }
+      'color.green': { $type: 'color', $value: '#008800', $extensions: { 'com.npmjs.package.design-tokenizer': { scss: { file: 'scss/modules/_colors.scss', variable: 'green' } } } },
+      'breakpoint.primary': { $type: 'dimension', $value: '40rem', $extensions: { 'com.npmjs.package.design-tokenizer': { scss: { file: 'scss/modules/_breakpoints.scss', variable: 'primary' } } } }
     }
-    renderSCSS(list, {
-      color: { file: 'scss/modules/_colors.scss', prefix: { remove: 'color' } },
-      '*': { file: 'scss/modules/_variables.scss' }
-    }, false)
+    renderSCSS(list, false)
     const color = readFileSync('scss/modules/_colors.scss', { encoding: 'utf8' })
-    const vars = readFileSync('scss/modules/_variables.scss', { encoding: 'utf8' })
-    expect(color).to.equal('$green: #008800;')
-    expect(vars).to.equal('$breakpoint-primary: 40rem;')
+    const vars = readFileSync('scss/modules/_breakpoints.scss', { encoding: 'utf8' })
+    expect(color).to.equal('$green: #008800; // color.green')
+    expect(vars).to.equal('$primary: 40rem; // breakpoint.primary')
   })
 })
--- lib/renderers/scss/render.ts
+++ lib/renderers/scss/render.ts
@@ -1,28 +1,54 @@
 import { writeFileSync } from 'fs'
 import DerefTokenList from '../../types/deref-token-list.js'
-import generateVariables from './generate-variables.js'
-import getOthers from '../../get-others.js'
 
-interface RenderSCSSOptions {
-  [key: string]: {
-    file: string
-    prefix?: {
-      add?: string
-      remove?: string
-    }
-  }
+import renderColorToken from './tokens/color.js'
+import renderDimensionToken from './tokens/dimension.js'
+import renderFontFamilyToken from './tokens/font-family.js'
+import renderFontWeightToken from './tokens/font-weight.js'
+import renderDurationToken from './tokens/duration.js'
+import renderCubicBezierToken from './tokens/cubic-bezier.js'
+import renderNumberToken from './tokens/number.js'
+import renderStrokeStyleToken from './tokens/stroke-style.js'
+import renderBorderToken from './tokens/border.js'
+import renderTransitionToken from './tokens/transition.js'
+import renderShadowToken from './tokens/shadow.js'
+import renderGradientToken from './tokens/gradient.js'
+import renderTypographyToken from './tokens/typography.js'
+
+import getExtension from '../extension.js'
+import getFiles from './get-files.js'
+
+const renderer: { [key: string]: Function } = {
+  color: renderColorToken,
+  dimension: renderDimensionToken,
+  fontFamily: renderFontFamilyToken,
+  fontWeight: renderFontWeightToken,
+  duration: renderDurationToken,
+  cubicBezier: renderCubicBezierToken,
+  number: renderNumberToken,
+  strokeStyle: renderStrokeStyleToken,
+  border: renderBorderToken,
+  transition: renderTransitionToken,
+  shadow: renderShadowToken,
+  gradient: renderGradientToken,
+  typography: renderTypographyToken
 }
 
-const renderSCSS = (list: DerefTokenList, options: RenderSCSSOptions = {}, verbose: boolean = true): void => {
-  const keys = Object.keys(options).filter(key => key !== '*')
-  for (const key in options) {
-    const { file, prefix } = options[key]
-    const l = key === '*' ? getOthers(list, keys) : list
-    const contents = generateVariables(l, { prefix: key === '*' ? '' : key, ...prefix })
-    writeFileSync(file, contents, { encoding: 'utf8' })
+const renderSCSS = (list: DerefTokenList, verbose: boolean = true): void => {
+  const files = getFiles(list)
+  const keys = Object.keys(list)
+  for (const file of files) {
+    const contents = keys.map(key => {
+      const token = list[key]
+      const { $type, $description } = token
+      const ext = getExtension(token)
+      return ext?.scss?.file === file
+        ? `${renderer[$type](ext?.scss?.variable, token) as string} // ${$description as string ?? key}`
+        : null
+    }).filter((item): item is string => item !== null)
+    writeFileSync(file, contents.join('\n'), { encoding: 'utf8' })
     if (verbose) console.log(`scss: Generating ${file}`)
   }
 }
 
 export default renderSCSS
-export { RenderSCSSOptions }

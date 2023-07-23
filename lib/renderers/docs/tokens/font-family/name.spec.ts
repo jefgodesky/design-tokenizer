2a4bb6ff15f7b4ad9044f2beb5a6a6403b2c424f
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 15:36:04 -0400

    getFontFamilyName: Use getColophonName

--- lib/renderers/docs/tokens/font-family/name.spec.ts
+++ lib/renderers/docs/tokens/font-family/name.spec.ts
@@ -7,26 +7,28 @@ describe('getFontFamilyName', () => {
     $type: 'fontFamily',
     $value: ['Helvetica', 'sans-serif'],
     $extensions: {
-      'com.github.jefgodesky.design-tokenizer': {
-        helvetica: {
-          name: 'Helvetica',
-          designer: 'Max Miedinger and Eduard Hoffmann',
-          url: 'https://www.linotype.com/1308886/helvetica-family.html'
+      'com.npmjs.package.design-tokenizer': {
+        colophon: {
+          helvetica: {
+            name: 'Helvetica',
+            designer: 'Max Miedinger and Eduard Hoffmann',
+            url: 'https://www.linotype.com/1308886/helvetica-family.html'
+          }
         }
       }
     }
   }
 
-  it('returns undefined if no name exists', () => {
+  it('returns null string if no name exists', () => {
     const token: DerefFontFamilyToken = { $type: 'fontFamily', $value: ['Helvetica', 'sans-serif'] }
-    expect(getFontFamilyName(token, 'Helvetica')).to.equal(undefined)
+    expect(getFontFamilyName(token, 'Helvetica')).to.equal('')
   })
 
   it('returns the name from a font family token if it exists', () => {
     expect(getFontFamilyName(helvetica, 'helvetica')).to.equal('Helvetica')
   })
 
-  it('returns undefined if the name requested does not exist', () => {
-    expect(getFontFamilyName(helvetica, 'Arial')).to.equal(undefined)
+  it('returns null string if the name requested does not exist', () => {
+    expect(getFontFamilyName(helvetica, 'Arial')).to.equal('')
   })
 })
--- lib/renderers/docs/tokens/font-family/name.ts
+++ lib/renderers/docs/tokens/font-family/name.ts
@@ -1,7 +1,6 @@
 import DerefFontFamilyToken from '../../../../types/tokens/dereferenced/font-family.js'
+import getColophonName from '../../types/colophon/name.js'
 
-const getFontFamilyName = (token: DerefFontFamilyToken, family: string): string | undefined => {
-  return token.$extensions?.['com.github.jefgodesky.design-tokenizer']?.[family]?.name
-}
+const getFontFamilyName = (token: DerefFontFamilyToken, family: string): string => getColophonName(token, family)
 
 export default getFontFamilyName

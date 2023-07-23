634eebd2cfca19f9f7e3c2a8529fe85b98ef0877
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 13:07:49 -0400

    getExtension: Take Token or DerefToken
    Not sure why Typescript keeps getting hung up on Token, when DerefToken is a strict subset of Token, but this seems to help it understand the situation a bit more clearly.

--- lib/renderers/extension.ts
+++ lib/renderers/extension.ts
@@ -1,7 +1,8 @@
-import Extension, { isExtension } from '../types/extension.js'
 import Token from '../types/token.js'
+import DerefToken from '../types/deref.js'
+import Extension, { isExtension } from '../types/extension.js'
 
-const getExtension = (token: Token): Extension | undefined => {
+const getExtension = (token: Token | DerefToken): Extension | undefined => {
   const domain = 'com.npmjs.package.design-tokenizer'
   const ext = token.$extensions?.[domain]
   return isExtension(ext) ? ext : undefined

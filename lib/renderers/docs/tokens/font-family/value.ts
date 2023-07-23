9b68cc1503d6ac5f0ee2f5ec44096d2e7c16bae4
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 15:41:21 -0400

    getFontFamilyValue: Use getFontFamily

--- lib/renderers/docs/tokens/font-family/value.ts
+++ lib/renderers/docs/tokens/font-family/value.ts
@@ -1,8 +1,6 @@
 import DerefFontFamilyToken from '../../../../types/tokens/dereferenced/font-family.js'
+import getFontFamily from '../../types/font-family/font-family.js'
 
-const getFontFamilyValue = (token: DerefFontFamilyToken): string => {
-  const { $value } = token
-  return Array.isArray($value) ? $value.join(', ') : $value
-}
+const getFontFamilyValue = (token: DerefFontFamilyToken): string => getFontFamily(token.$value)
 
 export default getFontFamilyValue

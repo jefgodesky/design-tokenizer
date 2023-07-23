63b73b8c6b4c8d8337d81c4d2f9c5dbbbc244d67
Jason Godesky <jefgodesky@gmail.com>
2023-07-23 18:16:52 -0400

    renderTypographyToken: Refactor to use getAbsoluteLineHeight

--- lib/renderers/scss/tokens/typography.spec.ts
+++ lib/renderers/scss/tokens/typography.spec.ts
@@ -5,11 +5,11 @@ import renderTypographyToken from './typography.js'
 describe('renderTypographyToken', () => {
   it('renders a typography token as a pair of SCSS variable', () => {
     const token: DerefTypographyToken = { $type: 'typography', $value: { fontFamily: ['Helvetica', 'sans-serif'], fontSize: '1rem', fontWeight: 'normal', letterSpacing: '0rem', lineHeight: 1.2 } }
-    expect(renderTypographyToken('body', token)).to.equal('$body: normal 1rem/1.2rem "Helvetica", sans-serif;\n$body-letter-spacing: 0rem;')
+    expect(renderTypographyToken('body', token)).to.equal('$body: normal 1rem/1.20rem "Helvetica", sans-serif;\n$body-letter-spacing: 0rem;')
   })
 
   it('renders a typography token with font style as a pair of SCSS variable', () => {
     const token: DerefTypographyToken = { $type: 'typography', $value: { fontFamily: ['Helvetica', 'sans-serif'], fontSize: '1rem', fontStyle: 'italic', fontWeight: 'normal', letterSpacing: '0rem', lineHeight: 1.2 } }
-    expect(renderTypographyToken('body', token)).to.equal('$body: italic normal 1rem/1.2rem "Helvetica", sans-serif;\n$body-letter-spacing: 0rem;')
+    expect(renderTypographyToken('body', token)).to.equal('$body: italic normal 1rem/1.20rem "Helvetica", sans-serif;\n$body-letter-spacing: 0rem;')
   })
 })
--- lib/renderers/scss/tokens/typography.ts
+++ lib/renderers/scss/tokens/typography.ts
@@ -1,14 +1,14 @@
 import DerefTypographyToken from '../../../types/tokens/dereferenced/typography.js'
+import getAbsoluteLineHeight from '../../shared/line-height-absolute.js'
 import transformFontFamily from '../transformers/font-family.js'
 
 const renderTypographyToken = (name: string, token: DerefTypographyToken): string => {
-  const { fontFamily, fontSize, fontStyle, fontWeight, letterSpacing, lineHeight } = token.$value
-  const units = fontSize.endsWith('rem') ? 'rem' : 'px'
-  const size = parseInt(fontSize.substring(0, fontSize.indexOf(units)))
+  const { fontFamily, fontSize, fontStyle, fontWeight, letterSpacing } = token.$value
+  const absLineHeight = getAbsoluteLineHeight(token)
   const elems = [
     fontStyle,
     fontWeight,
-    isNaN(size) ? fontSize : `${fontSize}/${size * lineHeight}${units}`,
+    `${fontSize}/${absLineHeight}`,
     transformFontFamily(fontFamily)
   ].filter(elem => elem !== undefined)
   return [

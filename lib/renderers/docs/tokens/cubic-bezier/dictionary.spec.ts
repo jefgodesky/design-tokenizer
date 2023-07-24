08ce6618fe2b713258abcf6c82caeadeaccd7957
Jason Godesky <jefgodesky@gmail.com>
2023-07-24 00:55:38 -0400

    Add new method: addCubicBezierTokenToDictionary

---
+++ lib/renderers/docs/tokens/cubic-bezier/dictionary.spec.ts
@@ -0,0 +1,62 @@
+import { expect } from 'chai'
+import DerefCubicBezierToken from '../../../../types/tokens/dereferenced/cubic-bezier.js'
+import addCubicBezierTokenToDictionary from './dictionary.js'
+
+describe('addCubicBezierTokenToDictionary', () => {
+  const before = {}
+  const token: DerefCubicBezierToken = { $type: 'cubicBezier', $value: [0, 0, 0, 0] }
+
+  it('doesn\'t have a description if the token doesn\'t', () => {
+    const actual = addCubicBezierTokenToDictionary('curve.flat', token, before)
+    expect(actual['curve.flat.description']).to.equal(undefined)
+  })
+
+  it('adds the description to the dictionary', () => {
+    const described = { ...token, $description: 'One second' }
+    const actual = addCubicBezierTokenToDictionary('curve.flat', described, before)
+    expect(actual['curve.flat.description']).to.equal('One second')
+  })
+
+  it('adds the cubic bézier to the dictionary', () => {
+    const actual = addCubicBezierTokenToDictionary('curve.flat', token, before)
+    expect(actual['curve.flat']).to.equal('0, 0, 0, 0')
+  })
+
+  it('adds a URL to cubic-bezier.com to the dictionary', () => {
+    const actual = addCubicBezierTokenToDictionary('curve.flat', token, before)
+    expect(actual['curve.flat.url']).to.equal('https://cubic-bezier.com/#0,0,0,0')
+  })
+
+  it('doesn\'t add an SCSS variable unless there is one', () => {
+    const actual = addCubicBezierTokenToDictionary('curve.flat', token, before)
+    expect(actual['curve.flat.scss']).to.equal(undefined)
+  })
+
+  it('adds an SCSS variable if there is one', () => {
+    const sassy = {
+      ...token,
+      $extensions: {
+        'com.npmjs.package.design-tokenizer': {
+          scss: { file: '_curves.scss', variable: 'flat' }
+        }
+      }
+    }
+
+    const actual = addCubicBezierTokenToDictionary('curve.flat', sassy, before)
+    expect(actual['curve.flat.scss']).to.equal('$flat')
+  })
+
+  it('incorporates modules into SCSS variable', () => {
+    const sassy = {
+      ...token,
+      $extensions: {
+        'com.npmjs.package.design-tokenizer': {
+          scss: { file: '_curves.scss', module: 'curves', variable: 'flat' }
+        }
+      }
+    }
+
+    const actual = addCubicBezierTokenToDictionary('curve.flat', sassy, before)
+    expect(actual['curve.flat.scss']).to.equal('curves.$flat')
+  })
+})
---
+++ lib/renderers/docs/tokens/cubic-bezier/dictionary.ts
@@ -0,0 +1,26 @@
+import rfdc from 'rfdc'
+import DerefCubicBezierToken from '../../../../types/tokens/dereferenced/cubic-bezier.js'
+import Dictionary from '../../../../types/dictionary.js'
+
+import getSCSSVariable from '../../scss.js'
+import getCubicBezierTokenString from './string.js'
+import getCubicBezierTokenURL from './url.js'
+
+const clone = rfdc()
+
+const addCubicBezierTokenToDictionary = (name: string, token: DerefCubicBezierToken, dict: Dictionary): Dictionary => {
+  const cpy = clone(dict)
+
+  const { $description } = token
+  if ($description !== undefined) cpy[name + '.description'] = $description
+
+  const scss = getSCSSVariable(token)
+  if (scss.length > 0) cpy[name + '.scss'] = scss
+
+  cpy[name] = getCubicBezierTokenString(token)
+  cpy[name + '.url'] = getCubicBezierTokenURL(token)
+
+  return cpy
+}
+
+export default addCubicBezierTokenToDictionary

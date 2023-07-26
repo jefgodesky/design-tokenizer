16e8390d0983901283cf9888e2c89e2c7573ee9a
Jason Godesky <jefgodesky@gmail.com>
2023-07-26 13:34:49 -0400

    renderHTML: Take optional additional dictionary

--- lib/renderers/html/render.spec.ts
+++ lib/renderers/html/render.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import mock from 'mock-fs'
 import { readFileSync } from 'fs'
 import DerefTokenList from '../../types/deref-token-list.js'
+import Dictionary from '../../types/dictionary.js'
 import renderHTML from './render.js'
 
 describe('renderHTML', () => {
@@ -18,7 +19,15 @@ describe('renderHTML', () => {
 
   it('renders HTML files', () => {
     const list: DerefTokenList = { 'color.green': { $type: 'color', $value: '#008800' } }
-    renderHTML(list, 'html/src', 'html/dist', false)
+    renderHTML(list, { indir: 'html/src', outdir: 'html/dist', verbose: false })
+    const actual = readFileSync('html/dist/index.html', { encoding: 'utf8' })
+    expect(actual).to.equal('<!DOCTYPE html><html><head><title>Test</title></head><body>#008800</body></html>')
+  })
+
+  it('can add to the dictionary', () => {
+    const list: DerefTokenList = {}
+    const add: Dictionary = { 'color.green.hex': '#008800' }
+    renderHTML(list, { indir: 'html/src', outdir: 'html/dist', add, verbose: false })
     const actual = readFileSync('html/dist/index.html', { encoding: 'utf8' })
     expect(actual).to.equal('<!DOCTYPE html><html><head><title>Test</title></head><body>#008800</body></html>')
   })
--- lib/renderers/html/render.ts
+++ lib/renderers/html/render.ts
@@ -1,11 +1,17 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs'
 import DerefTokenList from '../../types/deref-token-list.js'
+import Dictionary from '../../types/dictionary.js'
 import getDictionary from '../docs/dictionary.js'
 import getSwatches from './swatches.js'
 
-const renderHTML = (list: DerefTokenList, indir: string, outdir: string, verbose: boolean = true): void => {
-  const dict = getDictionary(list)
+const renderHTML = (list: DerefTokenList, options: { indir?: string, outdir?: string, add?: Dictionary, verbose?: boolean } = { verbose: true }): void => {
+  const { indir, outdir, add, verbose } = options
+  const dict = getDictionary(list, add)
   dict.swatches = getSwatches(list)
+
+  if (indir === undefined) console.error('html: Error producing HTML: input directory is undefined.')
+  if (outdir === undefined) console.error('html: Error producing HTML: output directory is undefined.')
+  if (indir === undefined || outdir === undefined) return
 
   const allFiles = readdirSync(indir, { withFileTypes: true })
   const files = allFiles.filter(file => /.*\.html?/i.test(file.name))
@@ -16,7 +22,7 @@ const renderHTML = (list: DerefTokenList, indir: string, outdir: string, verbose
       working = working.replaceAll(`{{ ${key} }}`, dict[key])
     }
     writeFileSync(`${outdir}/${file.name}`, working, { encoding: 'utf8' })
-    if (verbose) console.log(`html: Generating ${outdir}/${file.name}`)
+    if (verbose !== false) console.log(`html: Generating ${outdir}/${file.name}`)
   }
 }
 

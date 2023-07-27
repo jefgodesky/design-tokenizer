d2969fe123756e1949335f1b063c03a4c2db98f5
Jason Godesky <jefgodesky@gmail.com>
2023-07-27 10:32:23 -0400

    getSwatch: Add background color and high-contrast foreground color

--- lib/renderers/html/swatch.spec.ts
+++ lib/renderers/html/swatch.spec.ts
@@ -6,30 +6,30 @@ describe('getSwatch', () => {
   it('creates a swatch for a color token', () => {
     const token: DerefColorToken = { $type: 'color', $value: '#000000' }
     const swatch = getSwatch('color.black', token)
-    expect(swatch).to.equal('<section class="swatch color-black"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr></table></section>')
+    expect(swatch).to.equal('<section class="swatch color-black" style="background-color: #000000; color: #ffffff;"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr></table></section>')
   })
 
   it('creates a swatch for a different color token', () => {
     const token: DerefColorToken = { $type: 'color', $value: '#ffffff' }
     const swatch = getSwatch('color.white', token)
-    expect(swatch).to.equal('<section class="swatch color-white"><h4>color.white</h4><table><tr><th>Hex</th><td>#ffffff</td></tr><tr><th>RGBA</th><td>255, 255, 255, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 0, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 100, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 100, 1</td></tr></table></section>')
+    expect(swatch).to.equal('<section class="swatch color-white" style="background-color: #ffffff; color: #000000;"><h4>color.white</h4><table><tr><th>Hex</th><td>#ffffff</td></tr><tr><th>RGBA</th><td>255, 255, 255, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 0, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 100, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 100, 1</td></tr></table></section>')
   })
 
   it('uses $description for title if available', () => {
     const token: DerefColorToken = { $type: 'color', $value: '#000000', $description: 'Black' }
     const swatch = getSwatch('color.black', token)
-    expect(swatch).to.equal('<section class="swatch color-black"><h4>Black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr></table></section>')
+    expect(swatch).to.equal('<section class="swatch color-black" style="background-color: #000000; color: #ffffff;"><h4>Black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr></table></section>')
   })
 
   it('includes SCSS variable if available', () => {
     const token: DerefColorToken = { $type: 'color', $value: '#000000', $extensions: { 'com.npmjs.package.design-tokenizer': { scss: { file: '_colors.scss', variable: 'black' } } } }
     const swatch = getSwatch('color.black', token)
-    expect(swatch).to.equal('<section class="swatch color-black"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr><tr><th>SCSS</th><td>$black</td></tr></table></section>')
+    expect(swatch).to.equal('<section class="swatch color-black" style="background-color: #000000; color: #ffffff;"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr><tr><th>SCSS</th><td>$black</td></tr></table></section>')
   })
 
   it('includes module in SCSS variable if available', () => {
     const token: DerefColorToken = { $type: 'color', $value: '#000000', $extensions: { 'com.npmjs.package.design-tokenizer': { scss: { file: '_colors.scss', module: 'colors', variable: 'black' } } } }
     const swatch = getSwatch('color.black', token)
-    expect(swatch).to.equal('<section class="swatch color-black"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr><tr><th>SCSS</th><td>colors.$black</td></tr></table></section>')
+    expect(swatch).to.equal('<section class="swatch color-black" style="background-color: #000000; color: #ffffff;"><h4>color.black</h4><table><tr><th>Hex</th><td>#000000</td></tr><tr><th>RGBA</th><td>0, 0, 0, 1</td></tr><tr><th>CMYKA</th><td>0, 0, 0, 100, 1</td></tr><tr><th>HSLA</th><td>0°, 0, 0, 1</td></tr><tr><th>HSVA</th><td>0°, 0, 0, 1</td></tr><tr><th>SCSS</th><td>colors.$black</td></tr></table></section>')
   })
 })
--- lib/renderers/html/swatch.ts
+++ lib/renderers/html/swatch.ts
@@ -1,4 +1,6 @@
 import slugify from 'slugify'
+import { colord, extend } from 'colord'
+import a11yPlugin from 'colord/plugins/a11y'
 import DerefColorToken from '../../types/tokens/dereferenced/color.js'
 import getColorHex from '../docs/tokens/color/hex.js'
 import getColorRGBA from '../docs/tokens/color/rgba.js'
@@ -7,10 +9,15 @@ import getColorHSLA from '../docs/tokens/color/hsla.js'
 import getColorHSVA from '../docs/tokens/color/hsva.js'
 import getSCSSVariable from '../docs/scss.js'
 
+extend([a11yPlugin])
+
 const getSwatch = (name: string, token: DerefColorToken): string => {
   const slug: string = slugify(name.replaceAll('.', '-'))
   const title: string = token.$description ?? name
   const scss: string = getSCSSVariable(token)
+  const c1 = colord(token.$value).contrast('#000000')
+  const c2 = colord(token.$value).contrast('#ffffff')
+  const foreground = c2 > c1 ? '#ffffff' : '#000000'
 
   const rows = [
     ['Hex', getColorHex(token)],
@@ -22,7 +29,8 @@ const getSwatch = (name: string, token: DerefColorToken): string => {
 
   if (scss.length > 0) rows.push(['SCSS', scss])
   const body = rows.map(arr => `<tr><th>${arr[0]}</th><td>${arr[1]}</td></tr>`).join('')
-  return `<section class="swatch ${slug}"><h4>${title}</h4><table>${body}</table></section>`
+  const style = `background-color: ${token.$value}; color: ${foreground};`
+  return `<section class="swatch ${slug}" style="${style}"><h4>${title}</h4><table>${body}</table></section>`
 }
 
 export default getSwatch

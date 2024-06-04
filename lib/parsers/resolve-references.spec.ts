9a91563f6664a74d132c8373649c16e53fc6120c
Jason Godesky <jefgodesky@gmail.com>
2024-06-04 17:49:39 -0400

    Fix a bug that kept references in gradient stops from resolving

--- lib/parsers/resolve-references.spec.ts
+++ lib/parsers/resolve-references.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import DerefGradientToken from '../types/tokens/dereferenced/gradient.js'
 import DerefTypographyToken from '../types/tokens/dereferenced/typography.js'
 import resolveReferences from './resolve-references.js'
 
@@ -8,7 +9,8 @@ describe('resolveReferences', () => {
     'font.family.sans': { $type: 'fontFamily', $value: ['Helvetica', 'sans-serif'] },
     'color.accent': { $type: 'color', $value: '{color.green}' },
     'color.accent2': { $type: 'color', $value: '{color.accent}' },
-    'type.heading': { $type: 'typography', $value: { fontFamily: '{font.family.sans}', fontSize: '1rem', fontWeight: 'normal', letterSpacing: '0rem', lineHeight: 1.2 } }
+    'type.heading': { $type: 'typography', $value: { fontFamily: '{font.family.sans}', fontSize: '1rem', fontWeight: 'normal', letterSpacing: '0rem', lineHeight: 1.2 } },
+    'gradient.test': { $type: 'gradient', $value: [{ color: '{color.green}', position: 0 }, { color: '#ffffff', position: 1 }] }
   }
 
   it('resolves references in a flat token dictionary', () => {
@@ -16,5 +18,6 @@ describe('resolveReferences', () => {
     expect(actual['color.accent'].$value).to.equal(tokens['color.green'].$value)
     expect(actual['color.accent2'].$value).to.equal(tokens['color.green'].$value)
     expect((actual['type.heading'] as DerefTypographyToken).$value.fontFamily[0]).to.equal(tokens['font.family.sans'].$value[0])
+    expect((actual['gradient.test'] as DerefGradientToken).$value[0].color).to.equal(tokens['color.green'].$value)
   })
 })
--- lib/parsers/resolve-references.ts
+++ lib/parsers/resolve-references.ts
@@ -5,14 +5,22 @@ import resolveReference from './resolve-reference.js'
 
 const clone = rfdc()
 
+const resolveObjectReferences = (dictionary: any, obj: any): void => {
+  for (const key in obj) {
+    if (isReference(obj[key])) obj[key] = resolveReference(dictionary, obj[key] as string)
+  }
+}
+
 const resolveReferences = (dictionary: any): DerefTokenList => {
   const cpy = clone(dictionary)
   for (const key in cpy) {
     if (isReference(cpy[key].$value)) cpy[key].$value = resolveReference(cpy, cpy[key].$value as string)
-    if (cpy[key].$value instanceof Object) {
-      for (const k in cpy[key].$value) {
-        if (isReference(cpy[key].$value[k])) cpy[key].$value[k] = resolveReference(cpy, cpy[key].$value[k] as string)
+    if (Array.isArray(cpy[key].$value)) {
+      for (const obj of cpy[key].$value) {
+        resolveObjectReferences(cpy, obj)
       }
+    } else if (cpy[key].$value instanceof Object) {
+      resolveObjectReferences(cpy, cpy[key].$value)
     }
   }
   return cpy
